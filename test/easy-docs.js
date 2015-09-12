947550af828c2f91b272c6b8e064f66ef09b02aa
Jarid Margolin <jaridmargolin@gmail.com>
2015-09-12 15:41:23 -0700

    Add additional tests to suite.

--- test/easy-docs.js
+++ test/easy-docs.js
@@ -10,6 +10,7 @@
 
 // core
 var fs = require('fs');
+var path = require('path');
 
 // 3rd party
 var _      = require('easy-utils');
@@ -30,20 +31,126 @@ describe('easy-docs.js', function () {
   beforeEach(function () {
     this.easydocs = new Easydocs({
       root: './test/fixtures',
+      src: './src',
       theme: theme
     });
   });
 
 
   /* ---------------------------------------------------------------------------
-   * _renderDocs()
+   * _buildOpts()
    * -------------------------------------------------------------------------*/
 
-  describe('_renderDocs()', function () {
+  describe('_buildOpts()', function () {
 
-    it('Should return built documentation.', function (done) {
-      this.easydocs._renderDocs(function (err, docs) {
-        assert.equal(docs, '<h1>ModuleClass</h1>');
+    it('Should clone opts.', function () {
+      var original = { theme: {}, prop: 'val' };
+      var cloned   = this.easydocs._buildOpts(original);
+
+      original.prop = 'val2';
+      assert.equal(cloned.prop, 'val');
+    });
+
+    it('Should merge in defaults.', function () {
+      var opts = this.easydocs._buildOpts({ theme: {}, prop: 'val' });
+
+      assert.ok(opts.root);
+      assert.ok(opts.dest);
+      assert.ok(opts.docs);
+      assert.ok(opts.compile);
+      assert.ok(opts.data);
+      assert.ok(opts.contents);
+      assert.ok(opts.pages);
+    });
+
+    it('Should manually add helpers.', function () {
+      var helpers = {
+        help1: function () {},
+        help2: function () {}
+      };
+
+      var opts = this.easydocs._buildOpts({
+        theme: { helpers: helpers }
+      });
+
+      assert.deepEqual(opts.theme.helpers, helpers);
+      assert.notEqual(opts.theme.helpers, helpers);
+    });
+
+    it('Should standardize paths.', function () {
+      var opts = this.easydocs._buildOpts({
+        theme: {},
+        root: './test/fixtures',
+        dest: './dest',
+        docs: './build/docs',
+        src:  './src'
+      });
+
+      assert.equal(opts.root, path.join(__dirname, './fixtures'));
+      assert.equal(opts.dest, path.join(__dirname, './fixtures/dest'));
+      assert.equal(opts.docs, path.join(__dirname, './fixtures/build/docs'));
+      assert.equal(opts.src, path.join(__dirname, './fixtures/src'));
+    });
+
+  });
+
+
+  /* ---------------------------------------------------------------------------
+   * _addPages()
+   * -------------------------------------------------------------------------*/
+
+  describe('_addPages()', function () {
+
+    it('Should add `pages` to passed `data` prop of passed obj.', function (done) {
+      var pages = { 'page1': true };
+      var opts = { pages: pages, data: {} };
+
+      this.easydocs._addPages(opts, function () {
+        assert.deepEqual(opts.data.pages, pages);
+        done();
+      })
+    });
+
+  });
+
+
+  /* ---------------------------------------------------------------------------
+   * _addPkg()
+   * -------------------------------------------------------------------------*/
+
+  describe('_addPkg()', function () {
+
+    it('Should add `pkg` property to `opts.data`.', function (done) {
+      this.easydocs._addPkg(this.easydocs.opts, function (err) {
+        assert.equal(this.easydocs.opts.data.pkg.name, 'test');
+        done();
+      }.bind(this));
+    });
+
+    it('Should add `pkg` into existing `opts.data`.', function (done) {
+      this.easydocs.opts.data.existing = true;
+
+      this.easydocs._addPkg(this.easydocs.opts, function (err) {
+        assert.isTrue(this.easydocs.opts.data.existing);
+        done();
+      }.bind(this));
+    });
+
+  });
+
+
+  /* ---------------------------------------------------------------------------
+   * _addDocs()
+   * -------------------------------------------------------------------------*/
+
+  describe('_addDocs()', function () {
+
+    it('Should attach built documentation to `opts.contents` as `easydocs`.', function (done) {
+      var easydocs = this.easydocs;
+
+      easydocs._addDocs(easydocs.opts, function (err) {
+        var contents = easydocs.opts.contents.easydocs;
+        assert.equal(contents, '<h1>ModuleClass</h1>');
         done();
       });
     });
@@ -52,20 +159,121 @@ describe('easy-docs.js', function () {
 
 
   /* ---------------------------------------------------------------------------
-   * _createPages()
+   * _addData()
    * -------------------------------------------------------------------------*/
 
-  describe('_createPages()', function () {
+  describe('_addData()', function () {
+
+    it('Should add `pages`, and `pkg` to `opts.data`.', function (done) {
+      this.easydocs._addData(function () {
+        assert.ok(this.easydocs.opts.data.pages);
+        assert.ok(this.easydocs.opts.data.pkg);
+        done();
+      }.bind(this))
+    });
+
+  });
+
+
+  /* ---------------------------------------------------------------------------
+   * _mergeOpts()
+   * -------------------------------------------------------------------------*/
+
+  describe('_mergeOpts()', function () {
+
+    it('Should return an object with specied props merge with pass opts.', function () {
+      var opts = this.easydocs._mergeOpts({ src: './testing' }, ['src', 'root']);
+
+      assert.deepEqual(opts, {
+        'src': './testing',
+        'root': this.easydocs.opts.root
+      });
+    });
+
+  });
+
+
+  /* ---------------------------------------------------------------------------
+   * _buildPage()
+   * -------------------------------------------------------------------------*/
+
+  describe('_buildPage()', function () {
+
+    afterEach(function () {
+      rimraf.sync('./test/fixtures/docs');
+    });
+
+    it('Should write built page to specified dest.', function (done) {
+      var page = this.easydocs.opts.pages[0];
+
+      this.easydocs.opts.contents.easydocs = '<h1>ModuleClass</h1>';
+      this.easydocs._buildPage(page, function (err) {
+        var contents = fs.readFileSync('./test/fixtures/docs/index.html', 'utf8');
+
+        assert.equal(contents, '<h1>ModuleClass</h1>\n');
+        done();
+      });
+    });
+
+    it('Should add documention to contents if page src is specified.', function (done) {
+      var page = this.easydocs.opts.pages[0];
+      page.src = './lib/module-class-2.js';
+
+      this.easydocs._buildPage(page, function (err) {
+        var contents = fs.readFileSync('./test/fixtures/docs/index.html', 'utf8');
+
+        assert.equal(contents, '<h1>ModuleClass2</h1>\n');
+        done();
+      });
+    });
+
+  });
+
+
+  /* ---------------------------------------------------------------------------
+   * _createPage()
+   * -------------------------------------------------------------------------*/
+
+  describe('_createPage()', function () {
+
+    afterEach(function () {
+      rimraf.sync('./test/fixtures/docs');
+    });
+
+    it('Should write built page to specified dest.', function (done) {
+      var page = _.pick(this.easydocs.opts.pages[0], ['fileName', 'pageName',
+        'sections']);
+      var opts = _.pick(this.easydocs.opts, ['root', 'docs', 'dest', 'compile',
+        'data', 'contents', 'theme']);
+
+      this.easydocs.opts.contents.easydocs = '<h1>ModuleClass</h1>';
+      this.easydocs._createPage(page, opts, function () {
+        var contents = fs.readFileSync('./test/fixtures/docs/index.html', 'utf8');
+
+        assert.equal(contents, '<h1>ModuleClass</h1>\n');
+        done();
+      });
+    });
+
+  });
+
+
+  /* ---------------------------------------------------------------------------
+   * _buildPages()
+   * -------------------------------------------------------------------------*/
+
+  describe('_buildPages()', function () {
 
     afterEach(function () {
       rimraf.sync('./test/fixtures/docs');
     });
 
     it('Should write built pages to specified dest.', function (done) {
-      this.easydocs._createPages('easydocs', function (err) {
+      this.easydocs.opts.contents.easydocs = '<h1>ModuleClass</h1>';
+      this.easydocs._buildPages(function (err) {
         var contents = fs.readFileSync('./test/fixtures/docs/index.html', 'utf8');
 
-        assert.equal(contents, 'easydocs\n');
+        assert.equal(contents, '<h1>ModuleClass</h1>\n');
         done();
       });
     });
